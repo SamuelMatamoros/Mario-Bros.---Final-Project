34b9db744ea063118394d45c8979354e15ced75e
Samuel Matamoros <samuelmatamorosalonso@gmail.com>
2025-11-17 16:35:44 +0100

    Updated Tuck.draw() method to display correctly at the last conveyor depending on the difficulty

--- src/truck.py
+++ src/truck.py
@@ -12,7 +12,7 @@ class Truck:
         self.number_of_packages = 0
 
         # Methods
-        self.draw()
+        # self.draw()
 
     # loaded_boxes property
     @property
@@ -27,25 +27,25 @@ class Truck:
             raise ValueError("loaded_boxes must be between 0 and 8")
         self.__loaded_boxes = loaded_boxes
 
-    def draw(self):
+    def draw(self, level):
         """Draws the truck: fixed head, variable bed based on loaded_boxes."""
         # Draw truck head at fixed position (adjust x, y as needed)
         pyxel.blt(
             8,
-            6 * config.TILE_DIMENSION,
+            12*config.TILE_DIMENSION - level*config.TILE_DIMENSION + 4,
             *config.TRUCK_HEAD
         )
         # Draw beds/cargo
         pyxel.blt(
             8 + config.TILE_DIMENSION,
-            6 * config.TILE_DIMENSION,
+            12*config.TILE_DIMENSION - level*config.TILE_DIMENSION + 4,
             *config.TRUCK_BED[self.number_of_packages]
         )
         # Draw structure arround
         for i in range(5):
             pyxel.blt(i * 8,
-                      6 * config.TILE_DIMENSION + 4,
+                      12*config.TILE_DIMENSION - level*config.TILE_DIMENSION+8,
                       *config.HOR_HALF_PIPE)
         pyxel.blt(5 * 8,
-                  6 * config.TILE_DIMENSION + 4,
+                  12*config.TILE_DIMENSION - level * config.TILE_DIMENSION+8,
                   *config.L_PIPE)
